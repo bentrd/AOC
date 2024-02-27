d49f81ca246d9c9663408b727a697b6c75a254f1
bentrd <benjamin@tordjman.com>
2024-02-27 19:34:12 +0100

    first commit

---
+++ 1/solve.py
@@ -0,0 +1,6 @@
+import re
+
+with open('1/input.txt', 'r') as file:
+    data = file.read()
+
+print(sum(int(''.join(group)) for line in data.split('\n') if (matches := re.findall(r'^\D*(?:(\d).*(\d))|(?:((\d)))\D*$', line)) for group in matches if any(group)))
